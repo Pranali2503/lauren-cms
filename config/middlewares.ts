1f68d7237da8421debc868909452afa13701177a
Pranali2503 <pranali@agnotic.com>
2026-03-23 17:46:25 +0530

    added s3 media support

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,12 +1,34 @@
-export default [
-  'strapi::logger',
-  'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
-  'strapi::poweredBy',
-  'strapi::query',
-  'strapi::body',
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
-];
+export default ({ env }) => {
+  const cdnUrl = env('CDN_URL');
+  const mediaSources = ["'self'", 'data:', 'blob:', 'market-assets.strapi.io'];
+
+  if (cdnUrl) {
+    mediaSources.push(cdnUrl);
+  }
+
+  return [
+    'strapi::logger',
+    'strapi::errors',
+    {
+      name: 'strapi::security',
+      config: {
+        contentSecurityPolicy: {
+          useDefaults: true,
+          directives: {
+            'connect-src': ["'self'", 'https:'],
+            'img-src': mediaSources,
+            'media-src': mediaSources,
+            upgradeInsecureRequests: null,
+          },
+        },
+      },
+    },
+    'strapi::cors',
+    'strapi::poweredBy',
+    'strapi::query',
+    'strapi::body',
+    'strapi::session',
+    'strapi::favicon',
+    'strapi::public',
+  ];
+};
--- config/plugins.ts
+++ config/plugins.ts
@@ -1,1 +1,26 @@
-export default () => ({});
+export default ({ env }) => ({
+  upload: {
+    config: {
+      provider: 'aws-s3',
+      providerOptions: {
+        baseUrl: env('CDN_URL'),
+        s3Options: {
+          credentials: {
+            accessKeyId: env('AWS_ACCESS_KEY_ID'),
+            secretAccessKey: env('AWS_ACCESS_KEY_SECRET'),
+          },
+          region: env('AWS_REGION'),
+          params: {
+            Bucket: env('AWS_BUCKET'),
+            ACL: 'private',
+          },
+        },
+      },
+      actionOptions: {
+        upload: {},
+        uploadStream: {},
+        delete: {},
+      },
+    },
+  },
+});
